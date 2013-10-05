103a91da801875fc043c3f864bf1c49d4988fe34
Mattia Tommasone <tommasone@klan.it>
2013-10-05 17:04:17 +0200

    second

--- scripts/wedify-web.js
+++ scripts/wedify-web.js
@@ -1,1 +1,94 @@
-console.log('Hello world');
+var apigeeClientCreds = {
+	orgName: 'wedify',
+	appName: 'wedify'
+};
+
+var apigeeClient = new Apigee.Client(apigeeClientCreds);
+var apigeeUser;
+
+
+var tracks = new Usergrid.Collection({
+	type: 'tracks',
+	client: apigeeClient
+});
+
+apigeeClient.login('Raibaz', 'ambasciata', function(err) {
+	if(err) {
+		alert("Error logging in: " + err);
+	} else {
+		apigeeClient.getLoggedInUser(function(err, data, user) {
+        	if(err) {
+            	alert("Error getting logged user: " + err);
+           	} else {
+            	if (apigeeClient.isLoggedIn()){
+            		apigeeUser = user;               
+            	}
+            	updateTracklist();
+         	}
+        });
+	}
+});
+
+
+$(document).ready(function() {
+	$('#submit-playlist').click(doSubmitPlaylist);	
+	$(document).on('click', '.votelink', doUpvote);
+});
+
+function updateTracklist() {
+	tracks.fetch(
+	function() {
+		$('#tracks').empty();
+		$.each(tracks._list, function(index, val) {
+			console.log(val);
+			var trackVotes = val._data.votes;
+			if(trackVotes === undefined) {
+				trackVotes = 0;
+			}
+			$('#tracks').append('<li data-votes="' + trackVotes + '" id="' + val._data.uuid + '">' + val._data.artist + " - " + val._data.title + " (" + trackVotes + ') <a class="votelink">Upvote</a></li>');
+		});
+	}, function() {
+		console.log("Error fetching tracks :(");
+	});
+}
+
+function doUpvote(event) {
+	event.preventDefault();
+	var target = $(this);	
+	var parent = target.parent();
+	console.log(target.parent());
+	var track = new Usergrid.Entity({
+		client: apigeeClient,
+		data: {
+			type: 'track',
+			uuid: parent.attr('id'),
+			votes: parent.data('votes') + 1
+		}
+	});
+	track.save(function(err, result) {
+		if(err) {
+			console.log("Error updating votes");
+		} else {
+			console.log("Votes updated successfully");
+			updateTracklist();
+		}
+	});
+}
+
+function doSubmitPlaylist() {
+	console.log($('#playlist').val());
+	var playlist = {
+		type: 'playlist',
+		name: $('#playlist').val(),
+		spotify_id: $('#playlist').val()
+	};
+
+	apigeeClient.createEntity(playlist, function(err, result) {
+		if(err) {
+			alert('error creating entity: ' + err);			
+		} else {
+			alert('Playlist created successfully');
+			console.log(result);
+		}
+	});
+};
