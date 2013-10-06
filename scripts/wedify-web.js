7c4cd887c4e00c14698724d9fc66c85dc837aaa8
Mattia Tommasone <tommasone@klan.it>
2013-10-06 10:47:35 +0200

    third

--- scripts/wedify-web.js
+++ scripts/wedify-web.js
@@ -22,8 +22,7 @@ apigeeClient.login('Raibaz', 'ambasciata', function(err) {
            	} else {
             	if (apigeeClient.isLoggedIn()){
             		apigeeUser = user;               
-            	}
-            	updateTracklist();
+            	}            	
          	}
         });
 	}
@@ -33,19 +32,38 @@ apigeeClient.login('Raibaz', 'ambasciata', function(err) {
 $(document).ready(function() {
 	$('#submit-playlist').click(doSubmitPlaylist);	
 	$(document).on('click', '.votelink', doUpvote);
+	setInterval(updateNowPlaying, 3500);
+	setInterval(updateTracklist, 5000);
 });
 
+function updateNowPlaying() {
+	apigeeClient.getEntity({		
+		type: 'nowplaying',
+		name: 'nowplaying'		
+	}, function(err, resp) {
+		if(err) {
+			alert('error fetching now playing');
+		} else {
+			$('#np-artist').html(resp._data.artist);
+			$('#np-title').html(resp._data.title);
+		}
+	});
+}
+
 function updateTracklist() {
+	tracks.qs = {limit: '999', ql:'playcount eq 0 order by votes desc'};	
 	tracks.fetch(
 	function() {
+		console.log(tracks);
 		$('#tracks').empty();
-		$.each(tracks._list, function(index, val) {
-			console.log(val);
+		$.each(tracks._list, function(index, val) {			
 			var trackVotes = val._data.votes;
 			if(trackVotes === undefined) {
 				trackVotes = 0;
 			}
-			$('#tracks').append('<li data-votes="' + trackVotes + '" id="' + val._data.uuid + '">' + val._data.artist + " - " + val._data.title + " (" + trackVotes + ') <a class="votelink">Upvote</a></li>');
+			$('#tracks').append('<li data-votes="' + trackVotes + '" id="' + 
+								val._data.uuid + '">' + val._data.artist + " - " + val._data.title + " (" + trackVotes + ')' + 
+								' <a class="votelink"><img src="like_icon.png" alt="Upvote"/></a></li>');
 		});
 	}, function() {
 		console.log("Error fetching tracks :(");
@@ -80,7 +98,7 @@ function doSubmitPlaylist() {
 	var playlist = {
 		type: 'playlist',
 		name: $('#playlist').val(),
-		spotify_id: $('#playlist').val()
+		spotify_id: $('#playlist').val()		
 	};
 
 	apigeeClient.createEntity(playlist, function(err, result) {
